899da2f386355f6aabb320a33c4e56f9f3d69662
Kevin Thomas Greif <kgreif@gpatlas1.ps.uci.edu>
2021-06-21 14:55:13 -0700

    Delete unneeded files

--- dat/rel22p0/AntiKt10UFOCSSKSoftDropBeta100Zcut10.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-find /eos/atlas/atlascerngroupdisk/perf-jets/JSS/TopBosonTagAnalysisRel21/FlattenerOutput/ConstituentTaggers/ -path '*A14NNPDF23LO_jetjet*' -a -name '*.root' -type f > DijetAntiKt10UFOCSSKSoftDropBeta100Zcut10Jets.list
-find /eos/atlas/atlascerngroupdisk/perf-jets/JSS/TopBosonTagAnalysisRel21/FlattenerOutput/ConstituentTaggers/ -path '*_Zprime_tt*' -name \*.root -type f > TopAntiKt10UFOCSSKSoftDropBeta100Zcut10Jets.list
-
