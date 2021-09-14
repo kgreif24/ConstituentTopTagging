24d8fed07a44ed3633496654bf221928df544f14
kgreif <kgreif@uci.edu>
2021-09-14 15:52:38 -0700

    Refactored training code

--- kf_train.py
+++ kf_train.py
@@ -1,17 +1,17 @@
 """ kf_train.py - This script will build, train, and evaluate a network,
 implemented with the keras/energyflow packages. It will make use of the
-DataHandler class to quickly generate np arrays of the jet data. Script also
-splits data in train/validation, for use in k-fold cross validation.
+DataHandler class to quickly generate np arrays of the jet data split into k
+folds for cross validation. Also uses the build_model function in the models.py
+file.
 
 Author: Kevin Greif
-Last updated 8/16/21
+Last updated 9/13/21
 python3
 """
 
 import sys, os
 import gc
 import argparse
-import tracemalloc
 
 import energyflow as ef
 from energyflow.archs import EFN
@@ -25,43 +25,18 @@ import matplotlib
 # graphics
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-plt.style.use('~/mattyplotsalot/allpurpose.mplstyle')
+# plt.style.use('~/mattyplotsalot/allpurpose.mplstyle')
+plt.style.use('~/Documents/General Programming/mattyplotsalot/allpurpose.mplstyle')
 
 from data_handler import DataHandler
+import models
+
 
 # If GPU is available, print message
 print("\nStart model training script...")
 print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
 
-
-########################## Define Functions ##########################
-
-# Here we'll define some utility function to be used in training.
-# (Refactor this into a separate file please)
-
-def log_model_output(epoch, logs):
-    """ log_model_output - This function is meant to be passed to a 
-    keras Lambda callback class. It is not a pure function, and will
-    inherit its variables from the instance. It will plot
-    the model output over the validation set.
-    """
-
-    # Run model prediction over validation set
-    preds = model.predict(valid_data, batch_size=batch_size)
-
-    # Split model output into signal and background
-    preds_sig = preds[valid_labels[:,1] == 1, 1]
-    preds_bkg = preds[valid_labels[:,1] == 0, 1]
-
-    # Now log histograms to tensorboard
-    with file_writer.as_default():
-        tf.summary.histogram('signal', preds_sig, step=epoch)
-        tf.summary.histogram('background', preds_bkg, step=epoch)
-
-    gc.collect()
-
-
-########################## Parse Arguments ###########################
+########################## Setup ###########################
 
 parser = argparse.ArgumentParser()
 
@@ -87,20 +62,13 @@ parser.add_argument('--fold', default=1, type=int,
                     help='The fold being used in this particular job')
 args = parser.parse_args()
 
-###################### Set parameters for training #######################
-
-# Network parameters
-net_type = args.type
-
-# Training parameters
-num_epoch = args.numEpochs
-batch_size = args.batchSize
-checkpoint_filepath = args.checkDir
+####################### Data Handling ######################
 
 # Data parameters
-filepath = "/pub/kgreif/samples/sample_4p2M_nbpt.root"
+# filepath = "/pub/kgreif/samples/sample_4p2M_nbpt.root"
+filepath = "../Data/sample_1p5M_nbpt_test.root"
 
-if 'hl' in net_type:
+if 'hl' in args.type:
     input_branches = ['fjet_Tau1_wta', 'fjet_Tau2_wta', 'fjet_Tau3_wta', 'fjet_Split12',
                       'fjet_Split23', 'fjet_ECF1', 'fjet_ECF2', 'fjet_ECF3', 'fjet_C2',
                       'fjet_D2', 'fjet_Qw']
@@ -109,142 +77,38 @@ else:
                       'fjet_sortClusCenterRot_phi', 'fjet_sortClusNormByPt_e']
 
 extra_branches = ['fjet_match_weight_pt', 'fjet_pt']
-max_constits = args.maxConstits
-num_folds = args.numFolds
-fold = args.fold - 1    # -1 to make fold an index in array
-
-############################# Process Data ################################
 
 # Now build dhs and use them to plot all branches of interest
 print("Building data objects...")
 dhandler = DataHandler(filepath, "FlatSubstructureJetTree", input_branches,
-                       extras=extra_branches, max_constits=max_constits)
-dhandler.plot_branches(input_branches + extra_branches, directory="./plots/")
+                       extras=extra_branches, max_constits=args.maxConstits)
+# dhandler.plot_branches(input_branches + extra_branches, directory="./plots/")
 
-# Figure out sample shape
-sample_shape = tuple([batch_size]) + dhandler.sample_shape()
+# Get data
+print("\nFetching data...")
+train_arrs, valid_arrs = dhandler.get_data(
+    net_type=args.type,
+    num_folds=args.numFolds,
+    fold=args.fold - 1  # -1 to make number into array index
+)
 
-# Now make np arrays out of the data
-print("\nBuilding data arrays...")
-data_arrs = dhandler.np_arrays()
+########################## Get Model ########################
 
-# Make train/valid split
-print("\nSplitting data arrays...")
-data = np.array_split(data_arrs[0], num_folds, axis=0)
-labels = np.array_split(data_arrs[1], num_folds, axis=0)
-weight = np.array_split(data_arrs[2], num_folds, axis=0)
-valid_data = np.squeeze(data.pop(fold))
-valid_labels = np.squeeze(labels.pop(fold))
-valid_weight = np.squeeze(weight.pop(fold))
-valid_events = len(valid_weight)
-train_data = np.squeeze(np.concatenate(data, axis=0))
-train_labels = np.squeeze(np.concatenate(labels, axis=0))
-train_weight = np.squeeze(np.concatenate(weight, axis=0))
-train_events = len(train_weight)
-del data_arrs, data, labels, weight
+# To build some models we need to know the sample shape. This information can
+# be obtained from the data handler
+sample_shape = dhandler.sample_shape()
 
-print("Using fold", fold+1, "of", num_folds)
-print("Training events: ", train_events)
-print("Valid events: ", valid_events)
-print("Total events: ", train_events + valid_events)
-print(np.shape(train_data))
+# Now build model
+model = models.build_model(args.type, sample_shape, args)
 
-# Verify that there are no NaNs in data, labels, or weights
-assert not np.any(np.isnan(train_data))
-assert not np.any(np.isnan(valid_data))
-assert not np.any(train_data == -999)
-assert not np.any(valid_data == -999)
-assert np.shape(train_data)[0] == train_events
-
-############################# Build Model ##################################
-print("\nBuilding model...")
-
-if 'dnn' in net_type:
-    print("\n*** DNN model ***")
-
-    # Find input shape
-    input_shape = np.prod(sample_shape[1:])
-
-    # Get data into proper setting
-    train_data = train_data.reshape((train_events, input_shape))
-    valid_data = valid_data.reshape((valid_events, input_shape))
-    print("Shape of training data: ", np.shape(train_data))
-
-    # Build model
-    model = tf.keras.Sequential()
-    model.add(tf.keras.Input(shape=(input_shape,)))
-    for layer in args.nodes:
-        model.add(tf.keras.layers.Dense(layer, kernel_initializer='glorot_normal'))
-        # model.add(tf.keras.layers.BatchNormalization(axis=1))
-        model.add(tf.keras.layers.ReLU())
-    model.add(tf.keras.layers.Dense(2, kernel_initializer='glorot_normal', activation='softmax'))
-
-    # Compile model
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4),
-        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
-        metrics=[tf.keras.metrics.CategoricalAccuracy(name='acc')]
-    )
-
-    # Once model is built, print summary
-    model.summary()
-
-elif net_type == 'efn':
-
-    # Group data into pT and angular information (and delete old arrays)
-    train_data = [train_data[:,:,0], train_data[:,:,1:3]]
-    valid_data = [valid_data[:,:,0], valid_data[:,:,1:3]]
-
-    # Build model
-    model = ef.archs.EFN(
-        input_dim=2,
-        Phi_sizes=tuple(args.phisizes),
-        F_sizes=tuple(args.fsizes),
-        Phi_acts="relu",
-        F_acts="relu",
-        Phi_k_inits="glorot_normal",
-        F_k_inits="glorot_normal",
-        latent_dropout=0.0,
-        F_dropouts=0.0,
-        mask_val=0,
-        loss="categorical_crossentropy",
-        optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4),
-        output_act='softmax',
-        summary=True
-    )
-
-elif net_type == 'pfn':
-
-    # Data is already grouped into proper format for PFN, so just build model
-    model = ef.archs.PFN(
-        input_dim=4,
-        Phi_sizes=tuple(args.phisizes),
-        F_sizes=tuple(args.fsizes),
-        Phi_acts="relu",
-        F_acts="relu",
-        Phi_k_inits="glorot_normal",
-        F_k_inits="glorot_normal",
-        latent_dropout=0.0,
-        F_dropouts=0.0,
-        mask_val=0,
-        loss="categorical_crossentropy",
-        optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4),
-        output_act="softmax",
-        summary=True
-    )
-
-else:
-    raise ValueError("Model type is not known!")
-
-
-########################## Initial Evaluation ################################
+##################### Initial Evaluation ####################
 
 print("\nPre-train evaluation...")
-preds = model.predict(valid_data, batch_size=batch_size)
+preds = model.predict(valid_arrs[0], batch_size=args.batchSize)
 
 # Make a histogram of network output, separated into signal/background
-preds_sig = preds[valid_labels[:,1] == 1]
-preds_bkg = preds[valid_labels[:,1] == 0]
+preds_sig = preds[valid_arrs[1][:,1] == 1]
+preds_bkg = preds[valid_arrs[1][:,1] == 0]
 hist_bins = np.linspace(0, 1.0, 100)
 
 plt.clf()
@@ -267,7 +131,7 @@ earlystop_callback = tf.keras.callbacks.EarlyStopping(
 
 # Checkpoint callback
 check_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath,
+    filepath=args.checkDir,
     monitor='val_loss',
     mode='min',
     save_best_only=True
@@ -279,22 +143,16 @@ tboard_callback = tf.keras.callbacks.TensorBoard(
     histogram_freq=1
 )
 
-# Log model output callback
-output_callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=log_model_output)
-
-# Make file writer for use in writing custom histograms
-file_writer = tf.summary.create_file_writer('logs/validation')
-
 # Train model by calling fit
 print("\nTraining model...")
 train_hist = model.fit(
-    train_data,
-    train_labels,
-    epochs=num_epoch,
-    batch_size=batch_size,
-    sample_weight=train_weight,
-    validation_data=(valid_data, valid_labels, valid_weight),
-    callbacks=[earlystop_callback, check_callback, tboard_callback, output_callback],
+    train_arrs[0],
+    train_arrs[1],
+    epochs=args.numEpochs,
+    batch_size=args.batchSize,
+    sample_weight=train_arrs[2],
+    validation_data=(valid_arrs[0], valid_arrs[1], valid_arrs[2]),
+    callbacks=[earlystop_callback, check_callback, tboard_callback],
     verbose=2
 )
 
@@ -314,14 +172,14 @@ plt.savefig("./plots/loss.png", dpi=300)
 print("\nEvaluate best model...")
 
 # First load best checkpoint
-model = tf.keras.models.load_model(checkpoint_filepath)
+model = tf.keras.models.load_model(args.checkDir)
 
 # Predict
-preds = model.predict(valid_data, batch_size=batch_size)
+preds = model.predict(valid_arrs[0], batch_size=args.batchSize)
 
 # Make a histogram of network output, separated into signal/background
-preds_sig = preds[valid_labels[:,1] == 1,:]
-preds_bkg = preds[valid_labels[:,1] == 0,:]
+preds_sig = preds[valid_arrs[1][:,1] == 1,:]
+preds_bkg = preds[valid_arrs[1][:,1] == 0,:]
 hist_bins = np.linspace(0, 1.0, 100)
 
 plt.clf()
@@ -334,8 +192,8 @@ plt.title("Model output over validation set")
 plt.savefig("./plots/final_output.png", dpi=300)
 
 # Get ROC curve and AUC
-fpr, tpr, thresholds = metrics.roc_curve(valid_labels[:,1], preds[:,1])
-auc = metrics.roc_auc_score(valid_labels[:,1], preds[:,1])
+fpr, tpr, thresholds = metrics.roc_curve(valid_arrs[1][:,1], preds[:,1])
+auc = metrics.roc_auc_score(valid_arrs[1][:,1], preds[:,1])
 fprinv = 1 / fpr
 
 # Find background rejection at tpr = 0.5, 0.8 working points
---
+++ models.py
@@ -0,0 +1,125 @@
+""" models.py - This script defines a function that builds various types
+of keras or energyflow models based on a passed in argument list. Meant to be
+used with kf_train.py script.
+
+Author: Kevin Greif
+python3
+Last updated 9/14/21
+"""
+
+import energyflow as ef
+from energyflow.archs import EFN
+import tensorflow as tf
+import sklearn.metrics as metrics
+import numpy as np
+
+
+def build_model(net_type, sample_shape, arglist):
+    """ build_model - This function will build and return a keras model.
+    All model hyperparameters are controlled by the net_type string and the
+    arguments provided in arglist. When using, make sure that the arglist
+    contains all of the proper information to build the model.
+
+    Arguments:
+        net_type (str) - A string that gives the type of model to build
+        sample_shape (tuple) - The shape of each jet, used to infer the shape
+            of inputs to the DNN models.
+        arglist (obj) - The argument parser that contains all information for
+            this training run
+
+    Returns:
+        (obj) - A keras model ready to be trained
+    """
+
+    if 'dnn' in net_type:
+
+        # First infer input shape from sample shape
+        input_shape = np.prod(sample_shape)
+
+        # Build model
+        model = tf.keras.Sequential()
+        model.add(tf.keras.Input(shape=(input_shape,)))
+        for layer in arglist.nodes:
+            model.add(tf.keras.layers.Dense(layer, kernel_initializer='glorot_normal'))
+            # model.add(tf.keras.layers.BatchNormalization(axis=1))
+            model.add(tf.keras.layers.ReLU())
+        model.add(tf.keras.layers.Dense(2, kernel_initializer='glorot_normal', activation='softmax'))
+
+        # Compile model
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4),
+            loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+            metrics=[tf.keras.metrics.CategoricalAccuracy(name='acc')]
+        )
+
+        # Once model is built, print summary
+        model.summary()
+
+    elif net_type == 'efn':
+
+        model = ef.archs.EFN(
+            input_dim=2,
+            Phi_sizes=tuple(arglist.phisizes),
+            F_sizes=tuple(arglist.fsizes),
+            Phi_acts="relu",
+            F_acts="relu",
+            Phi_k_inits="glorot_normal",
+            F_k_inits="glorot_normal",
+            latent_dropout=0.0,
+            F_dropouts=0.0,
+            mask_val=0,
+            loss="categorical_crossentropy",
+            optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4),
+            output_act='softmax',
+            summary=True
+        )
+
+    elif net_type == 'pfn':
+
+        model = ef.archs.PFN(
+            input_dim=4,
+            Phi_sizes=tuple(arglist.phisizes),
+            F_sizes=tuple(arglist.fsizes),
+            Phi_acts="relu",
+            F_acts="relu",
+            Phi_k_inits="glorot_normal",
+            F_k_inits="glorot_normal",
+            latent_dropout=0.0,
+            F_dropouts=0.0,
+            mask_val=0,
+            loss="categorical_crossentropy",
+            optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4),
+            output_act="softmax",
+            summary=True
+        )
+
+    else:
+        raise ValueError("Model type is not known!")
+
+    return model
+
+
+########################## Train Functions ##########################
+
+# Here we'll define some utility function to be used in model training
+
+def log_model_output(epoch, logs):
+    """ log_model_output - This function is meant to be passed to a
+    keras Lambda callback class. It is not a pure function, and will
+    inherit its variables from the instance. It will plot
+    the model output over the validation set.
+    """
+
+    # Run model prediction over validation set
+    preds = model.predict(valid_data, batch_size=batch_size)
+
+    # Split model output into signal and background
+    preds_sig = preds[valid_labels[:,1] == 1, 1]
+    preds_bkg = preds[valid_labels[:,1] == 0, 1]
+
+    # Now log histograms to tensorboard
+    with file_writer.as_default():
+        tf.summary.histogram('signal', preds_sig, step=epoch)
+        tf.summary.histogram('background', preds_bkg, step=epoch)
+
+    gc.collect()
