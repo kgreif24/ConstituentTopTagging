220afd426e5f38bb3df5fcf3e9614d1f48796ec7
Kevin Thomas Greif <kgreif@gpatlas1.ps.uci.edu>
2021-12-17 10:25:58 -0800

    Updated make data lists script to new directory location

--- dat/makeDataList.sh
+++ dat/makeDataList.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-find /DFS-L/DATA/whiteson/kgreif/ConstituentTaggers/ -path '*A14NNPDF23LO_jetjet*' -a -name '*.root' -type f > DijetSamples.list
-find /DFS-L/DATA/whiteson/kgreif/ConstituentTaggers/ -path '*_Zprime_tt*' -name \*.root -type f > ZprimeTTSamples.list
+find /DFS-L/DATA/whiteson/kgreif/ModTaggingData/ -path '*A14NNPDF23LO_jetjet*' -a -name '*.root' -type f > DijetSamples.list
+find /DFS-L/DATA/whiteson/kgreif/ModTaggingData/ -path '*_Zprime_tt*' -name \*.root -type f > ZprimeTTSamples.list
 
