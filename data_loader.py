37fd3b0dc5a60bc739509d44b0cce8199171e684
Kevin Thomas Greif <kgreif@login-i15.local>
2022-11-30 02:25:14 -0800

    Updating dataloader for resnet trained on ln data

--- data_loader.py
+++ data_loader.py
@@ -30,7 +30,6 @@ class DataLoader(Sequence):
                  mode='train',
                  net_type='dnn',
                  max_constits=80,
-                 max_jets=-1,
                  num_folds=5,
                  this_fold=1):
         """ __init__ - Init function for this class. Will load in root file
@@ -45,7 +44,6 @@ class DataLoader(Sequence):
             valid gives only the valid fold, and test gives the entire dataset.
             net_type (string): Specifies the model specific data preparation to use
             max_constits (int): Number of constituents to include in constituent models
-            max_jets (int): Number of jets to use in train / valid sets
             num_folds (int): The number of folds data is split into
             this_fold (int): The number of the fold to use, 1-num_folds
 
@@ -70,18 +68,17 @@ class DataLoader(Sequence):
             self.sample_shape = self.file['hl'].shape[1:]
             self.load_taste = False
         elif self.net_type == 'resnet':
-            self.sample_shape = (64, 64, 1)  # Simply hard code image dims
+            self.sample_shape = (64, 64, 2)  # Simply hard code image dims
             self.load_taste = False
         elif self.net_type == 'dnn':
             self.sample_shape = (self.max_constits, 10)
             self.load_taste = True
         elif self.net_type == 'efn':
-            # How to give taste info to EFN?
             self.sample_shape = (self.max_constits, 2)
             self.load_taste = True
         elif self.net_type == 'pfn':
-            self.sample_shape = (self.max_constits, 10)
-            self.load_taste = True
+            self.sample_shape = (self.max_constits, 7)
+            self.load_taste = False
         elif self.net_type == 'pnet':
             self.sample_shape = (self.max_constits, 10)
             self.load_taste = True
@@ -184,7 +181,8 @@ class DataLoader(Sequence):
 
             # Resnet needs pt data for pixel intensity
             if self.net_type == 'resnet':
-                batch_pt = self.file['constit'][batch_indeces,:,0]
+                batch_pt = self.file['constit'][batch_indeces,:,2]
+                batch_en = self.file['constit'][batch_indeces,:,3]
 
             # We also need to load labels and weights, since this conditional will only
             # occur if we are training
@@ -207,7 +205,8 @@ class DataLoader(Sequence):
 
             # Again, resnet needs pt data for pixel intensity
             if self.net_type == 'resnet':
-                batch_pt = self.file['constit'][batch_start:batch_stop,:,0]
+                batch_pt = self.file['constit'][batch_start:batch_stop,:,2]
+                batch_en = self.file['constit'][batch_start:batch_stop,:,3]
 
             # Now load labels and weights the regular way
             batch_labels = self.file['labels'][batch_start:batch_stop]
@@ -255,12 +254,14 @@ class DataLoader(Sequence):
             eta_index = np.ravel(batch_data[:,:,0])
             phi_index = np.ravel(batch_data[:,:,1])
             cons_pt = np.ravel(batch_pt)
+            cons_en = np.ravel(batch_en)
 
             # Build images, starting from a zero array and incrementing. Note
             # we use phi as our row coordinate and index from the bottom of the array.
             # This strange indexing makes jet images intuitive.
-            shaped_data = np.zeros((this_bs, 64, 64, 1), dtype=np.float32)
+            shaped_data = np.zeros((this_bs, 64, 64, 2), dtype=np.float32)
             np.add.at(shaped_data, (jet_index, -1*phi_index, eta_index, 0), cons_pt)
+            np.add.at(shaped_data, (jet_index, -1*phi_index, eta_index, 0), cons_en)
 
         elif self.net_type == 'pnet':
 
