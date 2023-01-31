3f6159ff4a02cd148ef4c2d7759e359f33a29c23
Kevin Greif <kgreif@uci.edu>
2023-01-31 06:50:06 -0800

    Update r2h

--- Root2HDF5/r2h_syst.py
+++ Root2HDF5/r2h_syst.py
@@ -19,14 +19,13 @@ convert_dict = {
     'flatten': False,
     'source_list': './dat/dijet_data.list',
     'max_constits': 200,
-    'target_dir': './dataloc/int_dijet_s2_nominal/',
-    'name_stem': 'dijet_',
-    'n_targets': 50,
-    'total': 20000000,
-    'constit_func': pp.raw_preprocess,
-    'logs': True,
-    'syst_func': None,
-    'syst_loc': 'DFS-L/DATA/whiteson/kgreif/ModTaggingData/cluster_uncert_map_EM.root',
+    'target_dir': './dataloc/int_dijet_ln_esup/',
+    'name_stem': 'dijet_ln_esup_',
+    'n_targets': 100,
+    'total': 5000000,
+    'constit_func': pp.train_preprocess,
+    'syst_func': syst.energy_scale,
+    'syst_loc': '/DFS-L/DATA/whiteson/kgreif/ModTaggingData/cluster_uncert_map_EM.root',
     'cut_func': pu.common_cuts,
     'jet_func': None,
     's_constit_branches': [
@@ -39,11 +38,11 @@ convert_dict = {
     'hl_branches': [],
     'event_branches': [],
     't_constit_branches': [
-        'fjet_clus_eta', 'fjet_clus_phi', 'fjet_clus_pt', 'fjet_clus_E'
+        'fjet_clus_eta', 'fjet_clus_phi', 'fjet_clus_log_pt', 'fjet_clus_log_E',
+        'fjet_clus_lognorm_pt', 'fjet_clus_lognorm_E', 'fjet_clus_dR'
     ],
-    't_onehot_branches': ['fjet_clus_taste'],
-    't_onehot_classes': [3],
     't_jet_branches': ['fjet_pt', 'fjet_eta', 'fjet_phi', 'fjet_m'],
+    'images_branch': [],
     'cut_branches': [
         'fjet_truthJet_eta', 'fjet_truthJet_pt', 'fjet_numConstituents', 'fjet_m',
         'fjet_truth_dRmatched_particle_flavor', 'fjet_truth_dRmatched_particle_dR',
@@ -51,12 +50,13 @@ convert_dict = {
         'fjet_truthJet_ungroomedParent_GhostBHadronsFinalCount', 'fjet_ungroomed_truthJet_Split23',
         'fjet_ungroomed_truthJet_pt'
     ],
-    'mask_lim': 0.1
+    'unit_multiplier': 1.0,
+    'mask_lim': 100
 }
 
 # Build the class
 rc = RootConverter(convert_dict)
 
 # Run main programs
-rc.run()
+rc.run(direction='up')
 
